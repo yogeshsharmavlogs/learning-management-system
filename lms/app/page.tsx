cc781043b5ccf4dcf5587651319be50a5b94fb56
yogeshsharmavlogs <yogeshsharmavlogs>
2023-12-12 12:42:01 +0530

    set next-js version from 14 to 13.4.12

--- lms/app/page.tsx
+++ lms/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <p></p>;
+  return (
+    <>
+      <p className="text-3xl font-medium text-sky-700">Hello World</p>
+      <Button variant="destructive"> Click Me</Button>
+    </>
+  );
 }
